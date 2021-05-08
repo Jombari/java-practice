3a8d6099890087570c5cc47e2b9a9d4cdc351857
Joseph John Tombari <josephTombari@gmail.com>
2021-05-08 14:38:13 -0500

    linearSearch

--- src/mylibrary/BasicLibrary.java
+++ src/mylibrary/BasicLibrary.java
@@ -10,6 +10,15 @@ package mylibrary;
 
 public class BasicLibrary {
 
+
+    /**
+     * Binary Searh O(logn) array must be sorted
+     * @param arr the sorted array
+     * @param l left most index
+     * @param r right most index
+     * @param x number searching for
+     * @return index of the number or -1 if not found
+     */
     public static int binarySearch(int arr[],int l, int r, int x){
         if(r>=l){
             int mid = l + (r-l)/2;
@@ -22,6 +31,20 @@ public class BasicLibrary {
 
             return  binarySearch(arr,mid+1,r,x);
         }
+
+        return -1;
+    }
+
+    /**
+     * Linear search. O(n)
+     * @param arr the array
+     * @param x the number being searched for
+     * @return index of the number or -1 if not found.
+     */
+    public static int linearSearch(int[] arr, int x){
+        for(int i = 0; i< arr.length;i++ )
+            if(arr[i] == x)
+                return i;
 
         return -1;
     }
